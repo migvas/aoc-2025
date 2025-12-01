02380b1d4190389f845faa2654c0622a159501a9
Miguel Vasques <migas.v94@gmail.com>
2025-12-01 15:15:54 +0000

    refactor: Make the code simpler, removes ifs to make it more readable

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -38,10 +38,6 @@ func part1(rotations []string) {
 
 		dialPos = dialPos % 100
 
-		if dialPos < 0 {
-			dialPos += 100
-		}
-
 		if dialPos == 0 {
 			zeroes++
 		}
@@ -74,19 +70,21 @@ func part2(rotations []string) {
 		if fullRotations < 0 {
 			fullRotations = -fullRotations
 		}
-		if dialPos < 0 && startPos != 0 {
-			fullRotations += 1
+
+		// The product will check if both positions have different signs
+		// If they do then we passed 0 in between
+		if dialPos*startPos < 0 {
+			fullRotations++
 		}
 
 		dialPos = dialPos % 100
 
-		if dialPos < 0 {
-			dialPos += 100
-		}
-
+		// Other zeros come from rotating the dial multiple times to the left or right
 		if fullRotations > 0 {
 			zeroes += fullRotations
 		} else if dialPos == 0 {
+			// Special case, if the dial stops on zero then we have to add one ore since the
+			// rotation counter doesn't add this scenario
 			zeroes++
 		}
 	}
